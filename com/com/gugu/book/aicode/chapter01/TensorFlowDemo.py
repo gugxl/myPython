6d5559e57af31c1a8849d82df4939d1509ae8d0f
gugu <gu1230gu1230@gmail.com>
2023-09-01 09:50:04 +0800

    调整目录结构

--- com/com/gugu/book/aicode/chapter01/TensorFlowDemo.py
+++ com/com/gugu/book/aicode/chapter01/TensorFlowDemo.py
@@ -4,16 +4,20 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 # AI 版hello world
 def get_num_class():
+    # 建立两层Sequential类型神经网络
     model = Sequential()
+    # 第一层   input_dim = 1 只有一个输入;units=8  8个输出(可以简单理解神经元的个数)
     model.add(Dense(units=8, activation='relu', input_dim=1))
+    # 第二层 一个输出
     model.add(Dense(units=1, activation='sigmoid'))
+    # 进行配置 loss='mean_squared_error' 损失函数为平均方差； optimizer='sgd' 优化方式为sgd随机梯度下降
     model.compile(loss='mean_squared_error', optimizer='sgd')
-
+    # 设定训练集
     x = [1, 2, 3, 10, 20, -2, -10, -100, -5, -20]
     y = [1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-
+    # 调用model.fit函数训练, epochs=10 训练10次，batch_size=4 每次挑选4组数据
     model.fit(x, y, epochs=10, batch_size=4)
-
+    # 构建4个输入数据测试打印结果，model.predict进行预测
     test_x = [30, 40, -20, -60]
     test_y = model.predict(test_x)
 
